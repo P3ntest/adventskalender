acd936a8108618e383fc70ba16ec836c265e3812
Julius van Voorden <juliusvanvoorden@outlook.com>
2023-12-02 17:38:51 +0100

    Optimize Images

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vsharp from "vite-plugin-vsharp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vsharp.default({
+      includePublic: ["public/**/*"],
+    }),
+  ],
+});
